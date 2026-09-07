dc46b57743165eeaf9b66ff422da14175867769a
Chetan Sharma <chetanbth5@gmail.com>
2026-09-07 19:51:51 +0530

    Fix PostgreSQL database configuration

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -78,16 +78,6 @@ DATABASES = {
     )
 }
 
-#DATABASES = {
- #   'default': {
-  #      'ENGINE': 'django.db.backends.postgresql',
-   #     'NAME': os.getenv('DB_NAME', 'resume_analyzer'),
-   #     'USER': os.getenv('DB_USER', 'postgres'),
-   #     'PASSWORD': os.getenv('DB_PASSWORD', 'postgres'),
-   #     'HOST': os.getenv('DB_HOST', 'localhost'),
-    #    'PORT': os.getenv('DB_PORT', '5432'),
- #   }
-#}
 
 AUTH_USER_MODEL = 'users.User'
 
